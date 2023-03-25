21d31bb1bbd1e0735fc23819fd362d25d159ef45
CamillePB <camille.pedro.bueno@hotmail.com>
2023-03-24 22:04:34 -0300

    second commit

---
+++ WF_Jackpot/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WF_Jackpot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbsolpre = new System.Windows.Forms.Label();
+            this.lbluatent = new System.Windows.Forms.Label();
+            this.lbtitulo = new System.Windows.Forms.Label();
+            this.btnmoeda = new System.Windows.Forms.Button();
+            this.lbpremio = new System.Windows.Forms.Label();
+            this.lbtentativas = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.btnresgatar = new System.Windows.Forms.Button();
+            this.btninfo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbsolpre
+            // 
+            this.lbsolpre.Image = ((System.Drawing.Image)(resources.GetObject("lbsolpre.Image")));
+            this.lbsolpre.Location = new System.Drawing.Point(171, 470);
+            this.lbsolpre.Name = "lbsolpre";
+            this.lbsolpre.Size = new System.Drawing.Size(105, 98);
+            this.lbsolpre.TabIndex = 3;
+            this.lbsolpre.Click += new System.EventHandler(this.lbpremio_Click);
+            // 
+            // lbluatent
+            // 
+            this.lbluatent.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbluatent.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbluatent.Image = ((System.Drawing.Image)(resources.GetObject("lbluatent.Image")));
+            this.lbluatent.Location = new System.Drawing.Point(560, 470);
+            this.lbluatent.Name = "lbluatent";
+            this.lbluatent.Size = new System.Drawing.Size(105, 98);
+            this.lbluatent.TabIndex = 4;
+            this.lbluatent.Click += new System.EventHandler(this.lbtentativas_Click);
+            // 
+            // lbtitulo
+            // 
+            this.lbtitulo.AutoSize = true;
+            this.lbtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtitulo.Location = new System.Drawing.Point(331, 9);
+            this.lbtitulo.Name = "lbtitulo";
+            this.lbtitulo.Size = new System.Drawing.Size(165, 37);
+            this.lbtitulo.TabIndex = 5;
+            this.lbtitulo.Text = "JACKPOT";
+            this.lbtitulo.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btnmoeda
+            // 
+            this.btnmoeda.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnmoeda.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmoeda.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnmoeda.Image = ((System.Drawing.Image)(resources.GetObject("btnmoeda.Image")));
+            this.btnmoeda.Location = new System.Drawing.Point(240, 357);
+            this.btnmoeda.Name = "btnmoeda";
+            this.btnmoeda.Size = new System.Drawing.Size(340, 101);
+            this.btnmoeda.TabIndex = 6;
+            this.btnmoeda.Text = "Inserir Moeda";
+            this.btnmoeda.UseVisualStyleBackColor = false;
+            this.btnmoeda.Click += new System.EventHandler(this.btnmoeda_Click);
+            // 
+            // lbpremio
+            // 
+            this.lbpremio.AutoSize = true;
+            this.lbpremio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpremio.Location = new System.Drawing.Point(189, 509);
+            this.lbpremio.Name = "lbpremio";
+            this.lbpremio.Size = new System.Drawing.Size(65, 20);
+            this.lbpremio.TabIndex = 8;
+            this.lbpremio.Text = "R$ 0.00";
+            this.lbpremio.Click += new System.EventHandler(this.lbpremio_Click_1);
+            // 
+            // lbtentativas
+            // 
+            this.lbtentativas.AutoSize = true;
+            this.lbtentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtentativas.Location = new System.Drawing.Point(598, 504);
+            this.lbtentativas.Name = "lbtentativas";
+            this.lbtentativas.Size = new System.Drawing.Size(31, 33);
+            this.lbtentativas.TabIndex = 9;
+            this.lbtentativas.Text = "0";
+            this.lbtentativas.Click += new System.EventHandler(this.lbtentativas_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(436, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tentativas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Premio:";
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(132, 154);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(156, 151);
+            this.pb1.TabIndex = 12;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(538, 154);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(156, 151);
+            this.pb3.TabIndex = 13;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(340, 154);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(156, 151);
+            this.pb2.TabIndex = 13;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // btnresgatar
+            // 
+            this.btnresgatar.Location = new System.Drawing.Point(586, 389);
+            this.btnresgatar.Name = "btnresgatar";
+            this.btnresgatar.Size = new System.Drawing.Size(142, 43);
+            this.btnresgatar.TabIndex = 14;
+            this.btnresgatar.Text = "Resgatar";
+            this.btnresgatar.UseVisualStyleBackColor = true;
+            this.btnresgatar.Click += new System.EventHandler(this.btnresgatar_Click);
+            // 
+            // btninfo
+            // 
+            this.btninfo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btninfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninfo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btninfo.Location = new System.Drawing.Point(12, 9);
+            this.btninfo.Name = "btninfo";
+            this.btninfo.Size = new System.Drawing.Size(22, 24);
+            this.btninfo.TabIndex = 15;
+            this.btninfo.Text = "?";
+            this.btninfo.UseVisualStyleBackColor = false;
+            this.btninfo.Click += new System.EventHandler(this.btninfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(831, 623);
+            this.Controls.Add(this.btninfo);
+            this.Controls.Add(this.btnresgatar);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbtentativas);
+            this.Controls.Add(this.lbpremio);
+            this.Controls.Add(this.btnmoeda);
+            this.Controls.Add(this.lbtitulo);
+            this.Controls.Add(this.lbluatent);
+            this.Controls.Add(this.lbsolpre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbsolpre;
+        private System.Windows.Forms.Label lbluatent;
+        private System.Windows.Forms.Label lbtitulo;
+        private System.Windows.Forms.Button btnmoeda;
+        private System.Windows.Forms.Label lbpremio;
+        private System.Windows.Forms.Label lbtentativas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Button btnresgatar;
+        private System.Windows.Forms.Button btninfo;
+    }
+}
+
---
+++ WF_Jackpot/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Jackpot
+{
+    public partial class Form1 : Form
+    {
+        int cont = 7, resgate;
+        double premio = 0.25;
+        string ganhou = "Parabéns, ganhou";
+        string perdeu = "Ops,não foi dessa vez...";
+        string seventents = "Mais 7 tentativas para você";
+        string firststep = "1- O apostador insere uma moeda na máquina; 2- A máquina libera 7 tentativas; 3- Se o jogador fizer uma combinação de qualquer fruta, ele acumula 0,25 centavos de prêmio em cada combinação; 4- Toda vez que o jogador acumular 0,25 terá a opção de continuar apostando ou retirar o prêmio; 5- Se o jogador acumular 0,25 e optar por continuar apostando, ganhará mais 7 tentativas; 6- O jogo termina se o jogador retirar o prêmio ou se esgotar todas as suas tentativas; 7- Se esgotar todas as tentativas, todo o prêmio é perdido; 8- Se o jogador fizer uma combinação do número 7, ele ganha mais 7 tentativas, porém, não ganha os 0,25 centavos";
+       // int val1, val2, val3;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbimagem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbtentativas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbpremio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbtentativas_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmoeda_Click(object sender, EventArgs e)
+        {
+
+            lbtentativas.Text = "" + cont--;
+
+            Random sorteia = new Random();
+
+            /*val1 = sorteia.Next(8);
+            val2 = sorteia.Next(8);
+            val3 = sorteia.Next(8);*/
+
+
+            pb1.Load("img//" + sorteia.Next(8) + ".jpg");
+            pb2.Load("img//" + sorteia.Next(8) + ".jpg");
+            pb3.Load("img//" + sorteia.Next(8) + ".jpg");
+
+            /*if(val1 >=0 && val2 == val1 && val3 == val1)
+            {
+                pb1.Load("img//" + val1 + ".jpg");
+                pb2.Load("img//" + val2 + ".jpg");
+                pb3.Load("img//" + val3 + ".jpg");
+
+                
+            }*/
+
+            //private void Compara() { }
+            if (pb1 == pb2 && pb2 == pb1)
+            {
+                // Esse comando não funciona
+                if (cont == 0 && premio >= 0.25)
+                {
+
+                    cont = 7;
+
+                    DialogResult result;
+                    result = MessageBox.Show(Text = "" + seventents);
+                    if (result == System.Windows.Forms.DialogResult.Yes)
+                    {
+
+                        this.Close();
+                    }//mais 7 tentativas
+                }
+            } 
+            else
+            {
+
+                if (cont == 0 && premio == 0.00)
+                {
+                    lbpremio.Text = "R$" + premio;
+
+                    DialogResult result;
+                    result = MessageBox.Show(Text = "" + perdeu);
+                    if (result == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                    cont = 7;
+                    lbtentativas.Text = "" + cont;
+
+                }//perdeu
+            }
+
+           /* if (val1 == 7 && val2 == val1 && val3 == val1)
+            {
+                pb1.Load("img//7.jpg");
+                pb2.Load("img//7.jpg");
+                pb3.Load("img//7.jpg");
+            }*/
+
+            if (pb1==pb2 && pb2 == pb1) { // Esse comando não funciona
+                DialogResult result;
+                result = MessageBox.Show(Text = "" + seventents);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+
+                    this.Close();
+                };
+                cont = 7;
+            }
+        }// Mais 7 sentativas
+        
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lbpremio_Click_1(object sender, EventArgs e)
+        {
+            lbpremio.Text = "R$"+ premio;
+        }
+
+        private void btninfo_Click(object sender, EventArgs e)//IGNORA:
+        {
+            DialogResult result;
+            result = MessageBox.Show(Text = "" + firststep);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+
+               this.Close();
+            };
+        }
+
+        private void btnresgatar_Click(object sender, EventArgs e)
+        {
+            
+            resgate = 1;
+            if (cont >= 0 && resgate == 1 && premio >= 0.25)
+            {
+
+                DialogResult result;
+                result = MessageBox.Show(Text = "" + ganhou + " R$" + premio);
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                   
+                    this.Close();
+                };
+
+                premio = 0.00;
+                cont = 7;
+
+            }//ganhou
+        }
+    }
+}
